6efb45ebc70bd5f34a1d6e2d3da208b9e0a47d7b
user <user@M1-MacBook-Pro.local>
2022-11-14 09:02:51 +0900

    Translate comment into English

--- 1_long_speech_recog.py
+++ 1_long_speech_recog.py
@@ -5,47 +5,44 @@ import shutil
 import subprocess
 import speech_recognition as sr
 
-# 音声ファイルの分割
 import wave
 import math
 import struct
 from scipy import fromstring, int16
 
-# mp4から音声ファイルへの変換
+# Convert mp4 to wav with ffmpeg
 def mp4_to_wav(mp4f):
   wavf = mp4f.replace('.mp4', '.wav')
   subprocess.run(['ffmpeg', '-i', mp4f, wavf], 
                   encoding='utf-8', stdout=subprocess.PIPE)
   return wavf
 
-# 音声ファイルの分割(デフォルト30秒)
+# Split a voice file into 30 minute voice files
 def cut_wav(wavf,time=30):
-  # timeの単位は[sec]
-  # ファイルを読み出し
   wr = wave.open(wavf, 'r')
 
-  # waveファイルが持つ性質を取得
+  # Read wav information
   ch = wr.getnchannels()
   width = wr.getsampwidth()
   fr = wr.getframerate()
   fn = wr.getnframes()
   total_time = 1.0 * fn / fr
-  integer = math.floor(total_time) + 1  # 小数点以下切り捨て
-  t = int(time)  # 秒数[sec]
+  integer = math.floor(total_time) + 1
+  t = int(time)  # sec]
   frames = int(ch * fr * t)
   num_cut = int(integer//t)
 
-  # waveの実データを取得し、数値化
+  # Read frames
   data = wr.readframes(wr.getnframes())
   wr.close()
   X = fromstring(data, dtype=int16)
   
-  # wavファイルを削除
+  # Delete wav
   os.remove(wavf)
   
   outf_list = []
   for i in range(num_cut):
-      # 出力データを生成
+      # Generate output data
       output_dir = 'output/cut_wav/'
       os.makedirs(output_dir,exist_ok=True)
       outf = output_dir + str(i).zfill(3) + '.wav'
@@ -54,7 +51,7 @@ def cut_wav(wavf,time=30):
       Y = X[start_cut:end_cut]
       outd = struct.pack("h" * len(Y), *Y)
 
-      # 書き出し
+      # Write to output file
       ww = wave.open(outf, 'w')
       ww.setnchannels(ch)
       ww.setsampwidth(width)
@@ -62,57 +59,55 @@ def cut_wav(wavf,time=30):
       ww.writeframes(outd)
       ww.close()
       
-      # リストに追加
+      # Append to list
       outf_list.append(outf)
   
   return outf_list
 
-# 複数ファイルの音声のテキスト変換
+# Convert multiple speech files to text files
 def cut_wavs_str(outf_list):
   output_text = ''
-  # 複数処理
-  print('音声のテキスト変換')
+  print('Convert speech to text')
   for fwav in outf_list:
       print(fwav)
       r = sr.Recognizer()
       
-      # 音声->テキスト
+      # Convert
       with sr.AudioFile(fwav) as source:
           audio = r.record(source)
       text = r.recognize_google(audio, language='ja-JP')
       
-      # 各ファイルの出力結果の結合
+      # Concat multiple text files
       output_text = output_text + text + '\n'
-      # wavファイルを削除
+      # Delete wav
       #os.remove(fwav)
       
   return output_text
 
-
-# mp4からwavへの変換から音声のテキスト変換まで
+# Main routine
 def mp4_to_text(mp4f):
-  # 出力ディレクトリ
+  # Output directory
   shutil.rmtree('output/cut_wav/')
   os.makedirs('output/cut_wav/', exist_ok=True)
   
-  # 音声ファイルへの変換
+  # Convert video to voice
   wav_file = mp4_to_wav(mp4f)
   
-  # 音声ファイルの分割(デフォルト30秒)
+  # Split voice file
   cut_wavs = cut_wav(wav_file)
   
-  # 複数ファイルの音声のテキスト変換
+  # Convert speech to text
   out_text = cut_wavs_str(cut_wavs)
   
-  # テキストファイルへの入力
+  # Write text to a file
   #mp4f_name = os.path.basename(mp4f)
   #txt_file = 'output/' + mp4f_name.replace('.mp4', '.txt')
   txt_file = 'output/ja.txt'
-  print('テキスト出力')
+  print('Output text')
   print(txt_file)
   f = open(txt_file, 'w')
   f.write(out_text)
   f.close()
 
-# 変換の実行
+# Execution
 mp4_to_text(sys.argv[1])
